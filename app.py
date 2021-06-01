c513e2d5b5ed95e2383cc48384babc7f32981754
Shriharan <74127448+tonyshree18@users.noreply.github.com>
2021-06-01 14:34:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import instaloader  # importing the module
+
+mod = instaloader.Instaloader()
+
+user = input("Enter your Instagram username: ")     # getting the user name from the user
+
+mod.download_profile(user, profile_pic_only=True)   # downloading the profile picture
